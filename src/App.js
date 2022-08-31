bd1491a5e880060b17f336ef5fbc73dea5f028cb
bappasahabapi2@gmail.com <bappasahabapi2@gmail.com>
2022-08-31 10:26:10 +0600

    user fetch is working

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Counter from './features/counter/Counter';
 import Posts from './features/posts/Posts';
+import { UserView } from './features/users/UserView';
 
 function App() {
   return (
     <>
-    
+    <UserView/>
+    <hr/>
+    <hr/>
+    <br></br>
     <Posts/>
     <Counter/>
     
